95ecf2f3be0af876fb5492a8e9f703ab79ecee69
jeffjc97 <jeffreychang@college.harvard.edu>
2015-11-07 06:41:57 -0500

    facebook addon

--- Extension/newsfeed.js
+++ Extension/newsfeed.js
@@ -9,12 +9,26 @@ scroll = 500;
 document.addEventListener("scroll", function() {
 	if($(document).scrollTop() > (multiples + 1) * scroll) {
 		last = $('._5jmm').last();
-		if(!last.next().hasClass('swag')) {
-			$("<div class='col-md-4 image-container-col'>\
-				<img class='display-image-bottom img-responsive img-thumbnail' src='http://weknowyourdreams.com/images/paradise/paradise-01.jpg'/>\
-				<div class='display-image-top'>CHEAP PRICE OF 19.99!\
-				</div>\
-				</div>").insertAfter(last);		}
+		if(!last.next().hasClass('jetblue-wrapper')) {
+			$("<div class='jetblue-wrapper'>\
+			<div class='header-text'>JetBlue Travel Suggestions</div>\
+			<div class='jb jb-1'>\
+				<img class='image' src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Egretta_garzetta_2015-06-17.jpg/1000px-Egretta_garzetta_2015-06-17.jpg'>\
+				<div class='loc'>Bahamas</div>\
+				<div class='price'>as low as $50</div>\
+			</div>\
+			<div class='jb jb-2'>\
+				<img class='image' src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Egretta_garzetta_2015-06-17.jpg/1000px-Egretta_garzetta_2015-06-17.jpg'>\
+				<div class='loc'>Jamaica</div>\
+				<div class='price'>as low as $20</div>\
+			</div>\
+			<div class='jb jb-3'>\
+				<img class='image' src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Egretta_garzetta_2015-06-17.jpg/1000px-Egretta_garzetta_2015-06-17.jpg'>\
+				<div class='loc'>Japan</div>\
+				<div class='price'>as low as $30</div>\
+			</div>\
+			</div>").insertAfter(last);
+		}
 		multiples++;
 	}
 });
