bc52392fe122987786c9dfdbccf28881b4bf741a
jeffjc97 <jeffreychang@college.harvard.edu>
2015-11-08 05:56:54 -0500

    custom page

--- Web/yhack jetblue/jb/urls.py
+++ Web/yhack jetblue/jb/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
+	# url(r'^static/(?P<path>.*)$', 'django.contrib.staticfiles.views', {'document_root': settings.STATIC_ROOT}),
 	url(r'^', include('api.urls')),
 )
