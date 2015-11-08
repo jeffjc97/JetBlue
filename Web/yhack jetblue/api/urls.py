89a2a30718d042e03f36f1b4e174b6be10f5bd35
jeffjc97 <jeffreychang@college.harvard.edu>
2015-11-08 05:05:48 -0500

    static files...

--- Web/yhack jetblue/api/urls.py
+++ Web/yhack jetblue/api/urls.py
@@ -6,4 +6,5 @@ from api import views
 urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^query/(?P<airports>(.*))/option/(?P<option>\d{1})/', views.query, name='query'),
+	url(r'^details/(?P<id>\d*)/', views.details, name='details'),
 )
